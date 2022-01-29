5ef42e2869458bcfa8636ee45b110080d2dc5c81
Santiago11234 <santiagogeb@gmail.com>
2022-01-29 17:33:27 -0600

    why dont work uhhhh

--- main.js
+++ main.js
@@ -52,3 +52,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+
+let btnUpVote = document.querySelector('#upVote');
+let input = document.querySelector('#input');
+let btnDownVote = document.querySelector('#downVote');
+
+
+btnDownVote.upVoteEventListener('click', () =>{
+    input.value = parseInt(input.value) - 1;
+});
+btnUpVote.upVoteEventListener('click', () =>{
+    input.value = parseInt(input.value) + 1;
+});
