d3f622e3dc7ab527305f63eb860d699e5d8a37fe
Santiago11234 <santiagogeb@gmail.com>
2022-01-29 14:25:52 -0600

    yes

---
+++ rating.js
@@ -0,0 +1,11 @@
+let btnUpVote = document.querySelector("#upVote");
+let input = document.querySelector("#input");
+let btnDwonVote = document.querySelector("#downVote");
+
+
+btnDownVote.upVoteEventListener('click', () =>{
+    input.value = parseInt(input.value) - 1;
+});
+btnUpVote.upVoteEventListener('click', () =>{
+    input.value = parseInt(input.value) + 1;
+});
