d45222886b9dc437f61757f192a4a7999bd8c421
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2022-02-06 00:30:01 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    
+        <body>
+    
+        <link rel="stylesheet" href="style.css" >
+
+        
+
+        <div class="section-1 box">
+
+            
+
+            <header>
+            <div class="wrapper"> 
+                <nav>
+                <ul>
+                    <li><a href="Gallery.html" class="button">Gallery</a></li>
+                    <li><a href="Submissions.html" class="button">Submissions</a></li>
+                    <li><a href="aboutUs.html" class="button">About Us</a></li>
+                    <li><a href="signUp.html" class="button">Sign Up</a></li>
+                    <li><a href="LoginPage.html" class="button">Login</a></li>
+                </ul>
