d0c17eb5e954098dc7a7fae083079f95db4298cc
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2022-02-06 00:40:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    
+        <body>
+    
+        <link rel="stylesheet" href="style.css" >
+
+        
+
+        <div class="section-1 box">
+
+            
+
+            <header>
+            <div class="wrapper"> 
+                <nav>
+                <ul>
+                    <li><a href="Gallery.html" class="button">Gallery</a></li>
+                    <li><a href="Submissions.html" class="button">Submissions</a></li>
+                    <li><a href="aboutUs.html" class="button">About Us</a></li>
+                    <li><a href="signUp.html" class="button">Sign Up</a></li>
+                    <li><a href="LoginPage.html" class="button">Login</a></li>
+                </ul>
+            </div>
+            </nav>
+                    <h2>
+                        <br />
+                        
+                        <font size="5" font color="black">
+                            A place for people to display their artwork and see what others have created
+                    </h2>
+                    <img src="logo.jpeg" alt="logo" class="center" margin-bottom="-50%">
+                
+            </header>
+            
+            
+        </div>
+
+        <!-- <div class="section-2 box">
+
+        </div> -->
+
+        <div class="section-3 box">
+            <h2>
+                <br />
+                
+                <font size="10" font color="white">
+                    What is the goal of VirtuGal?
+            </h2>
+            <font size="5" font color="black"  >
+
+                <p>
+                    <font face="Didot">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    After the pandemic, many people lost their hobbies, their friends, and their ability to have fun showcasing their art. VirtuGal focuses on giving those people a chance to have fun again. Upload an image, enter a contest, and compete.
+                </p>
+            </font>
+            
+        </div>
+
+        <div class="section-4 box">
+
+        </div>
+
+        </body>
+    
+    
+</html> 
+
+
