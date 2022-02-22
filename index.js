d24dd348d0f77093e6984e40994cc9f581d0654d
Brian Chen <brianchen831@gmail.com>
2022-02-22 10:05:10 -0600

    help

--- index.js
+++ index.js
@@ -44,4 +44,12 @@ function logout(){
   firebase.auth().signOut();
 }
 
+$(document).ready(function() {
+  $('.progress-bar').waypoint(function() {
+  $('.progress-bar').css({
+  animation: "animate-positive 2s",
+  opacity: "1"
+  });
+  }, { offset: '75%' });
 
+})
