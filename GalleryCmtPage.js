b24ae44c3626bb0868e91441c181f16cd0b8d2e6
Brian Chen <brianchen831@gmail.com>
2022-03-06 13:36:13 -0600

    THIS ISNT WORKING

---
+++ GalleryCmtPage.js
@@ -0,0 +1,64 @@
+function click1(){
+    const cmtPg1 = document.getElementById("comments-page-1");
+    const cmtPg2 = document.querySelector("#comments-page-2");
+    const cmtPg3 = document.querySelector("#comments-page-3");
+    const cmtPg4 = document.querySelector("#comments-page-4");
+    const cmtPg5 = document.querySelector("#comments-page-5");
+    cmtPg1.classList.remove("form--hidden");
+    cmtPg2.classList.add("form--hidden");
+    cmtPg3.classList.add("form--hidden");
+    cmtPg4.classList.add("form--hidden");
+    cmtPg5.classList.add("form--hidden");
+}
+
+function click2(){
+    const cmtPg1 = document.getElementById("comments-page-1");
+    const cmtPg2 = document.querySelector("#comments-page-2");
+    const cmtPg3 = document.querySelector("#comments-page-3");
+    const cmtPg4 = document.querySelector("#comments-page-4");
+    const cmtPg5 = document.querySelector("#comments-page-5");
+    cmtPg1.classList.add("form--hidden");
+    cmtPg2.classList.remove("form--hidden");
+    cmtPg3.classList.add("form--hidden");
+    cmtPg4.classList.add("form--hidden");
+    cmtPg5.classList.add("form--hidden");
+}
+
+function click3(){
+    const cmtPg1 = document.getElementById("comments-page-1");
+    const cmtPg2 = document.querySelector("#comments-page-2");
+    const cmtPg3 = document.querySelector("#comments-page-3");
+    const cmtPg4 = document.querySelector("#comments-page-4");
+    const cmtPg5 = document.querySelector("#comments-page-5");
+    cmtPg1.classList.add("form--hidden");
+    cmtPg2.classList.add("form--hidden");
+    cmtPg3.classList.remove("form--hidden");
+    cmtPg4.classList.add("form--hidden");
+    cmtPg5.classList.add("form--hidden");
+}
+
+function click4(){
+    const cmtPg1 = document.getElementById("comments-page-1");
+    const cmtPg2 = document.querySelector("#comments-page-2");
+    const cmtPg3 = document.querySelector("#comments-page-3");
+    const cmtPg4 = document.querySelector("#comments-page-4");
+    const cmtPg5 = document.querySelector("#comments-page-5");
+    cmtPg1.classList.add("form--hidden");
+    cmtPg2.classList.add("form--hidden");
+    cmtPg3.classList.add("form--hidden");
+    cmtPg4.classList.remove("form--hidden");
+    cmtPg5.classList.add("form--hidden");
+}
+
+function click5(){
+    const cmtPg1 = document.getElementById("comments-page-1");
+    const cmtPg2 = document.querySelector("#comments-page-2");
+    const cmtPg3 = document.querySelector("#comments-page-3");
+    const cmtPg4 = document.querySelector("#comments-page-4");
+    const cmtPg5 = document.querySelector("#comments-page-5");
+    cmtPg1.classList.add("form--hidden");
+    cmtPg2.classList.add("form--hidden");
+    cmtPg3.classList.add("form--hidden");
+    cmtPg4.classList.add("form--hidden");
+    cmtPg5.classList.remove("form--hidden");
+}
